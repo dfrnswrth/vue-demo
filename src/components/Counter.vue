472ed5a4abb44128147b5686883d3672792f78a8
Dylan Farnsworth <dylan@dylanfarnsworth.com>
2017-02-10 16:06:10 -0500

    initial

---
+++ src/components/Counter.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>{{ counter }}</h1>
+    <button @click='increment'>+</button>
+    <button @click='decrement'>-</button>
+    <button @click='reset'>0</button>
+  </div>
+</template>
+
+<script>
+import './Counter';
+
+export default {
+  name: 'counter',
+  data() {
+    return {
+      counter: 0
+    }
+  },
+  methods: {
+    increment() {
+      this.counter++;
+    },
+    decrement() {
+      this.counter--;
+    },
+    reset() {
+      this.counter = 0;
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
